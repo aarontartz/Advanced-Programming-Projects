9c287f815ed75612d22d2b5205a6dce38f3750c5
Aaron Tartz <166546889+aarontartz@users.noreply.github.com>
2024-04-11 01:34:17 -0700

    Update main.py

--- Least Squares Regression/main.py
+++ Least Squares Regression/main.py
@@ -23,8 +23,6 @@ ydata = data_array[:, 1]
 popt = curve_fit(func, xdata, ydata)[0]
 pcov = curve_fit(func, xdata, ydata)[1]
 
-#print(f'popt:{popt}')
-
 plt.rcParams["figure.figsize"] = [7.50, 3.50]
 plt.rcParams["figure.autolayout"] = True
 plt.scatter(xdata, ydata, color='blue', marker="o")
